78421c87bf5061e3e384b8b75e0333543b128ff9
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-02-02 01:00:14 -0500

    Adding temporary link to make it easier to redirect to the document id page

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Documents = () => {
   return (
-    <div>Documents</div>
+    <div> 
+      <h1 className='text-4xl'>
+      All Documents
+      </h1>
+      <Link className=' text-2xl bg-blue-400 flex justify-center' href='/documents/123'>
+      Go to Id Document
+        </Link>
+    </div>
   )
 }
 
