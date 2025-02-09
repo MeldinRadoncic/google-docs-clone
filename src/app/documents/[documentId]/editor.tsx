23fc2e2be6352ea9e76bc0fe88fbb816ab8aa89f
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-02-08 23:42:32 -0500

    Importing ListItem extension

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -9,6 +9,9 @@ import StarterKit from "@tiptap/starter-kit";
 import Color from "@tiptap/extension-color";
 import Link from "@tiptap/extension-link";
 import Highlight from "@tiptap/extension-highlight";
+import BulletList from '@tiptap/extension-bullet-list'
+import ListItem from '@tiptap/extension-list-item'
+import ListKeymap from '@tiptap/extension-list-keymap'
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
 import Table from "@tiptap/extension-table";
@@ -49,6 +52,16 @@ export const Editor = () => {
         protocols: ["https"],
         
       }),
+      ListItem,
+      ListKeymap.configure({
+        listTypes: [
+            {
+                itemName: 'listItem',
+                wrapperNames: ['bulletList', 'orderedList'],
+              },
+        ],
+      }),
+      
       Color,
       Highlight.configure({
         multicolor: true,
@@ -109,9 +122,7 @@ export const Editor = () => {
     },
 
     content: `
-    <p>This is a basic example of implementing images. Drag to re-order.</p>
-    <img src="https://placehold.co/800x400" />
-    <img src="https://placehold.co/800x400/6A00F5/white" />
+    
   `,
   });
 
