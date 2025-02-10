ec33ff6311e29f21296380d9d0cbefa80bc7d63e
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-02-09 21:32:24 -0500

    Documnets component imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import { Button } from '@/components/ui/button';
+import Documents from '@/app/documents/page';
 
 
 const Home = () => {
 
   return(
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl">Hello Meldin</h1>
-      <Button>
-        Click Me
-        </Button>
+      
+        <Documents />
     </main>
   )
 }
