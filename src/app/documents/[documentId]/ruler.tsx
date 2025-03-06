afcb2036ea3475256930ef395d6a9b32b8017a80
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-03-06 17:22:36 -0500

    Overflow ruler marker bug fixed. When Zoom the right marker is not going to move

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -119,10 +119,10 @@ export const Ruler = () => {
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
-      className='h-6 border-b border-gray-300 w-[816px] mx-auto flex items-end relative select-none print:hidden'>
+      className='w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden'>
       <div
         id='ruler-container'
-        className='w-[816px] h-full relative mx-auto'>
+        className=' h-full relative w-full'>
         <Marker
           position={leftMargin}
           isLeft={true}
