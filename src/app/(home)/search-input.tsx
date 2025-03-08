587e5b3b14a9f56d352b3609770279b9b22cb4e1
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-03-08 11:34:07 -0500

    Added some functionalities, handleClear, handleChange

--- src/app/(home)/search-input.tsx
+++ src/app/(home)/search-input.tsx
@@ -1,14 +1,39 @@
+"use client";
+import {
+  useState,
+  useRef,
+} from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { SearchIcon } from "lucide-react";
+import {
+  SearchIcon,
+  XIcon,
+} from "lucide-react";
 
 export const SearchInput = () => {
+  const [value, setValue] =
+    useState("");
+  const inputRef =
+    useRef<HTMLInputElement>(null);
+  // On Change function
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setValue(e.target.value);
+  };
+
+  // Handle Clear function
+  const handleClear = () => {
+    setValue("");
+    inputRef.current?.blur();
+  };
   return (
     <div className='flex-1 flex items-center justify-center'>
-      <form
-        action=''
-        className='relative max-w-[720px] w-full'>
+      <form className='relative max-w-[720px] w-full'>
         <Input
+          ref={inputRef}
+          value={value}
+          onChange={handleChange}
           placeholder='Search'
           className='md:text-base placeholder:text-natural-800 px-14 w-full border-none focus-visible:shadow-[0 0 0 2px rgba(66,153,225,0.5)] bg-[#F9FAFB] rounded-full h-[48px] focus-visible:ring-0 focus-bg-white'
         />
@@ -19,6 +44,16 @@ export const SearchInput = () => {
           className='absolute left-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full'>
           <SearchIcon size={24} />
         </Button>
+        {value && (
+          <Button
+            type='button'
+            variant='ghost'
+            size='icon'
+            onClick={handleClear}
+            className='absolute right-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full'>
+            <XIcon size={24} />
+          </Button>
+        )}
       </form>
     </div>
   );
