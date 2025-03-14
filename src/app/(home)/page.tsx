60c5fb2a8a1be2e3410cbb9401e2ae38c30fbe2f
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-03-14 12:18:35 -0400

    Fetching all the documents

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,20 @@
-import Documents from "@/app/documents/page";
+"use client";
+
 import { Navbar } from "@/app/(home)/navbar";
 import { TemplatesGallery } from "@/app/(home)/templates-gallery";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+  const documents = useQuery(
+    api.documents.get,
+  );
+  if (document === undefined) {
+    <p>Loading...</p>;
+  }
+
+  console.log("DOCUMENTS ", documents);
+
   return (
     <main className='flex min-h-screen flex-col'>
       <div className='fixed top-0 left-0 right-0 h-16 z-10 bg-white p-4'>
@@ -12,6 +24,11 @@ const Home = () => {
       <div className='mt-16'>
         <TemplatesGallery />
       </div>
+      {documents?.map((document) => (
+        <div key={document._id}>
+          {document.title}
+        </div>
+      ))}
     </main>
   );
 };
