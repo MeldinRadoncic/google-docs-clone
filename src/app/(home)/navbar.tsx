761aca101f653d4999a11f779162ab1dcb7ee3f0
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-03-14 21:15:31 -0400

    UserButton added to the navbar for sign out

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,21 +1,27 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { SearchInput } from './search-input';
-
+import Link from "next/link";
+import Image from "next/image";
+import { SearchInput } from "./search-input";
+import { UserButton } from "@clerk/nextjs";
 
 export const Navbar = () => {
+  return (
+    <nav className='flex items-center justify-between h-full w-full'>
+      <div className='flex gap-3 items-center shrink-0 pr-6'>
+        <Link href='/'>
+          <Image
+            src='/logo.svg'
+            height={36}
+            width={36}
+            alt='Logo'
+          />
+        </Link>
+        <h3 className='text-xl'>
+          Docs
+        </h3>
+      </div>
 
-    return(
-        <nav className='flex items-center justify-between h-full w-full'>
-            <div className="flex gap-3 items-center shrink-0 pr-6">
-                <Link href='/'>
-                <Image src='/logo.svg' height={36} width={36} alt='Logo'/>
-                </Link>
-                <h3 className="text-xl">Docs</h3>
-            </div>
-            
-            <SearchInput />
-            <div />
-        </nav>
-    )
+      <SearchInput />
+      <UserButton />
+    </nav>
+  );
 };
