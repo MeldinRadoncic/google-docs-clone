94d2d78226ce4273b93d1815f5f38bd385d5c47e
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-03-16 06:21:12 -0400

    create function added

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,4 +1,6 @@
-import { query } from "./_generated/server";
+import { ConvexError } from "convex/values";
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 // Get all documents API endpoint
 export const get = query({
@@ -8,3 +10,24 @@ export const get = query({
       .collect();
   },
 });
+
+// Create a document API endpoint
+export const create = mutation({
+  args: { title: v.optional(v.string()), initialContent: v.optional(v.string())},
+  handler: async ( ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+  if(!user) {
+    throw new ConvexError("Unauthenticated");
+  }
+
+  return await ctx.db.insert("documents", {
+    title: args.title ?? "Untitled Document",
+    ownerId: user.subject,
+    initialContent: args.initialContent,
+  });
+  } 
+
+  
+ 
+});
+
