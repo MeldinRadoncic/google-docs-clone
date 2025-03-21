2b8a4ced70422c7b80ed9f1fdd875f029cb18db4
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-03-21 12:51:09 -0400

    Remove api created

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,11 +1,17 @@
 import { ConvexError } from "convex/values";
-import { query, mutation } from "./_generated/server";
+import {
+  query,
+  mutation,
+} from "./_generated/server";
 import { v } from "convex/values";
 import { paginationOptsValidator } from "convex/server";
 
 // Get all documents API endpoint
 export const get = query({
-  args: { paginationOpts : paginationOptsValidator },
+  args: {
+    paginationOpts:
+      paginationOptsValidator,
+  },
   handler: async (ctx, args) => {
     return await ctx.db
       .query("documents")
@@ -15,21 +21,67 @@ export const get = query({
 
 // Create a document API endpoint
 export const create = mutation({
-  args: { title: v.optional(v.string()), initialContent: v.optional(v.string())},
-  handler: async ( ctx, args) => {
-    const user = await ctx.auth.getUserIdentity();
-  if(!user) {
-    throw new ConvexError("Unauthenticated");
-  }
+  args: {
+    title: v.optional(v.string()),
+    initialContent: v.optional(
+      v.string(),
+    ),
+  },
+  handler: async (ctx, args) => {
+    const user =
+      await ctx.auth.getUserIdentity();
+    if (!user) {
+      throw new ConvexError(
+        "Unauthenticated",
+      );
+    }
 
-  return await ctx.db.insert("documents", {
-    title: args.title ?? "Untitled Document",
-    ownerId: user.subject,
-    initialContent: args.initialContent,
-  });
-  } 
-
-  
- 
+    return await ctx.db.insert(
+      "documents",
+      {
+        title:
+          args.title ??
+          "Untitled Document",
+        ownerId: user.subject,
+        initialContent:
+          args.initialContent,
+      },
+    );
+  },
 });
 
+// Remove a document API endpoint
+export const removeById = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const user =
+      await ctx.auth.getUserIdentity(); // Get the user identity
+
+    // Check if the user is authenticated
+    if (!user) {
+      throw new ConvexError(
+        "Unauthenticated",
+      );
+    }
+    // Get the document by ID
+    const document = await ctx.db.get(
+      args.id,
+    );
+
+    // Check if the document exists
+    if (!document) {
+      throw new ConvexError(
+        "Document not found",
+      );
+    }
+    // Check if the document owner is the same as the user
+    if (
+      document.ownerId !== user.subject
+    ) {
+      throw new ConvexError(
+        "Unauthorized",
+      );
+    }
+    await ctx.db.delete(args.id); // Delete the document
+  },
+});
