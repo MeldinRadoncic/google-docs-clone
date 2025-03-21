dcce6e836f053b4e365ff8f8ad877a7e5d254756
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-03-21 12:54:23 -0400

    RemoveDigalog component created

---
+++ src/components/remove-dialog.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useState } from "react";
+import { Id } from "../../convex/_generated/dataModel";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+interface REmoveDialogProps {
+  documentId: Id<"documents">;
+  children: React.ReactNode;
+}
+
+export const RemoveDialog = ({
+  documentId,
+  children,
+}: REmoveDialogProps) => {
+  const [isRemoving, setIsRemoving] =
+    useState(false);
+
+  // Call the removeById mutation from the documents API
+  const remove = useMutation(
+    api.documents.removeById,
+  );
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent
+        onClick={(e) =>
+          e.stopPropagation()
+        }>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be
+            undone. Are you sure you
+            want to delete this
+            document?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel
+            onClick={(e) =>
+              e.stopPropagation()
+            }>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction
+            disabled={isRemoving}
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsRemoving(true);
+              remove({
+                id: documentId,
+              }).finally(() => {
+                setIsRemoving(false);
+              });
+            }}>
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
