0ef2f0b7636b3796a9b1a472df46109deea4e363
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-03-23 16:53:18 -0400

    Pagination added

--- src/app/(home)/documents-table.tsx
+++ src/app/(home)/documents-table.tsx
@@ -1,58 +1,92 @@
-import { PaginationStatus } from 'convex/react';
-import { Doc } from '../../../convex/_generated/dataModel';
-import { LoaderIcon } from 'lucide-react';
-import { DocumentRow } from './document-row';
+import { PaginationStatus } from "convex/react";
+import { Doc } from "../../../convex/_generated/dataModel";
+import { LoaderIcon } from "lucide-react";
+import { DocumentRow } from "./document-row";
 
 import {
-    Table,
-    TableHeader,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow,
-} from '@/components/ui/table';
+  Table,
+  TableHeader,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
 interface DocumentsTableProps {
-    documents: Doc<'documents'>[] | undefined;
-    loadMore: (numItems: number) => void;
-    status: PaginationStatus;
+  documents:
+    | Doc<"documents">[]
+    | undefined;
+  loadMore: (numItems: number) => void;
+  status: PaginationStatus;
 }
 
-export const DocumentsTable = ({ documents, status, loadMore }: DocumentsTableProps) => {
-    return (
-        <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-5">
-            {documents === undefined ? (
-                <div className="flex justify-center items-center h-24">
-                    <LoaderIcon className="animate-spin text-muted-foreground size-5" />
-                </div>
-            ) : (
-                <Table>
-    <TableHeader>
-        <TableRow className="border-none">
-            <TableHead className="w-[50px]"></TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead className="hidden md:table-cell">Shared</TableHead>
-            <TableHead className="hidden md:table-cell">Created At</TableHead>
-            <TableHead className="w-[50px] text-right">Actions</TableHead>
-        </TableRow>
-    </TableHeader>
-
-    <TableBody>
-        {documents.length === 0 ? (
-            <TableRow>
-                <TableCell colSpan={5} className="text-center h-24">
-                    No documents found
-                </TableCell>
-            </TableRow>
-        ) : (
-            documents.map((document) => (
-               <DocumentRow key={document._id} document={document} />
-            ))
-        )}
-    </TableBody>
-</Table>
-
-            )}
+export const DocumentsTable = ({
+  documents,
+  status,
+  loadMore,
+}: DocumentsTableProps) => {
+  return (
+    <div className='max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-5'>
+      {documents === undefined ? (
+        <div className='flex justify-center items-center h-24'>
+          <LoaderIcon className='animate-spin text-muted-foreground size-5' />
         </div>
-    );
+      ) : (
+        <Table>
+          <TableHeader>
+            <TableRow className='border-none'>
+              <TableHead className='w-[50px]'></TableHead>
+              <TableHead>
+                Name
+              </TableHead>
+              <TableHead className='hidden md:table-cell'>
+                Shared
+              </TableHead>
+              <TableHead className='hidden md:table-cell'>
+                Created At
+              </TableHead>
+              <TableHead className='w-[50px] text-right'>
+                Actions
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {documents.length === 0 ? (
+              <TableRow>
+                <TableCell
+                  colSpan={5}
+                  className='text-center h-24'>
+                  No documents found
+                </TableCell>
+              </TableRow>
+            ) : (
+              documents
+                .map((document) => (
+                  <DocumentRow
+                    key={document._id}
+                    document={document}
+                  />
+                ))
+                .reverse()
+            )}
+          </TableBody>
+        </Table>
+      )}
+      <div className='flex items-center justify-center'>
+        <Button
+          variant='ghost'
+          size='sm'
+          onClick={() => loadMore(5)}
+          disabled={
+            status !== "CanLoadMore"
+          }>
+          {status === "CanLoadMore"
+            ? "Load More"
+            : "End of List"}
+        </Button>
+      </div>
+    </div>
+  );
 };
---
+++ src/components/edit-dialog.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { useState } from "react";
+import { Id } from "../../convex/_generated/dataModel";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+interface EditDialogProps {
+  documentId: Id<"documents">;
+  initialTitle: string;
+  children: React.ReactNode;
+}
+
+export const EditDialog = ({
+  documentId,
+  children,
+  initialTitle,
+}: EditDialogProps) => {
+  const [isEditing, setIsEditing] =
+    useState(false);
+  const [title, setTitle] = useState(
+    initialTitle,
+  );
+  const [open, setOpen] =
+    useState(false);
+
+  // Call the removeById mutation from the documents API
+  const update = useMutation(
+    api.documents.updateById,
+  );
+
+  // onSubmit handler
+  const onSubmit = async (
+    e: React.FormEvent<HTMLFormElement>,
+  ) => {
+    e.preventDefault();
+    setIsEditing(true);
+    update({
+      id: documentId,
+      title:
+        title.trim() ||
+        "Untitled Document",
+    })
+      .then(() => {
+        setOpen(false);
+      })
+      .finally(() => {
+        setIsEditing(false);
+      });
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        {children}
+      </DialogTrigger>
+      <DialogContent
+        onClick={(e) =>
+          e.stopPropagation()
+        }>
+        <form
+          onSubmit={onSubmit}
+          className='className'>
+          <DialogHeader>
+            <DialogTitle>
+              Edit Document
+            </DialogTitle>
+            <DialogDescription>
+              Edit the document here.
+            </DialogDescription>
+          </DialogHeader>
+          <div className='my-4'>
+            <Input
+              value={title}
+              onChange={(e) =>
+                setTitle(e.target.value)
+              }
+              placeholder='Document Title'
+              disabled={isEditing}
+              onClick={(e) =>
+                e.stopPropagation()
+              }
+            />
+          </div>
+          <DialogFooter>
+            <Button
+              type='button'
+              variant='ghost'
+              disabled={isEditing}
+              onClick={(e) => {
+                e.stopPropagation();
+                setOpen(false);
+              }}>
+              Cancel
+            </Button>
+            <Button
+              type='submit'
+              disabled={isEditing}
+              onClick={(e) =>
+                e.stopPropagation()
+              }>
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
