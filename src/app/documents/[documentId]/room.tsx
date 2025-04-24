7ef2d4b6f1704a9ef06eb21d50a4af7bae03bc3c
Meldin Radoncic <meldinradoncic89@gmail.com>
2025-04-24 07:47:20 -0400

    Added tags amd comments between individuals in the same organization

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -1,30 +1,80 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { ReactNode } from "react";
+import { FullScreenLoader } from "@/components/full-screen-loader";
 import {
   LiveblocksProvider,
   RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import { useParams } from "next/navigation";
+import { getUser } from "./actions";
+import { toast } from "sonner";
+
+
+type User = {
+  name: string;
+  avatar: string;
+}
 
 export function Room({
   children,
 }: {
   children: ReactNode;
 }) {
+  const [users, setUsers ] = useState<User[]>([]);
   const params = useParams();
+
+  const fetchUser = async () => {
+    try{
+      const list = await getUser();
+      setUsers(list);
+
+    }catch(e){
+      toast.error("Failed to fetch user");
+      console.error(e);
+    }
+
+  }
+
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+
+
+
   return (
     <LiveblocksProvider
       throttle={16}
       authEndpoint='/api/liveblocks-auth'>
+      resolveUsers={({ userIds }) => {
+  return userIds.map((userId) => users.find((user) => user.id === userId)) ?? undefined;
+}}
+
+resolveMentionSuggestions={({text}) => {
+  let filteredUsers = users;
+
+  if(text) {
+    filteredUsers = users.filter((user) => user.name.toLowerCase().includes(text.toLowerCase()))
+  }
+
+}}
+
+
+   
+
+
+        resolveRoomsInfo = {() => []}
       <RoomProvider
         id={
           params.documentId as string
         }>
         <ClientSideSuspense
           fallback={
-            <div>Loading…</div>
+            <FullScreenLoader label='Room Loading...' />
           }>
           {children}
         </ClientSideSuspense>
